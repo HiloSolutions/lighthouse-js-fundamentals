16f8d5f2aa2bbf2ce4103d753b707853fd10c743
Lauren Johnston <hilotraining@outlook.com>
2022-12-11 13:22:43 -0700

    Loopy Lighthouse project completed

--- chorus-while.js
+++ chorus-while.js
@@ -3,9 +3,12 @@ const chorus = "Let's dance!";
 let repeat = 0; 
 
 while (repeat <10) { //When the value of repeat reaches 10, our while condition is no longer true and so JavaScript stops executing the contents of the block following the while statement and moves on.
-  
+  if(repeat === 5) {
+    console.log("*change key*"); //if we're worried our song outro is too repetitive we might decide to change the key after five repetitions.
+  }
   console.log(chorus);
   repeat ++; //repeat++ is shorthand for repeat = repeat + 1
 }
 
 console.log("Until the sun comes up!");
+
