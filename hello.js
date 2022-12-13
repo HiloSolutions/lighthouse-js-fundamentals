0680a8fb641a5c02c75e7fc04c32c6a5b995b023
Lauren Johnston <hilotraining@outlook.com>
2022-12-13 15:45:59 -0700

    more updates on function expression tinkering with iseven file.

--- hello.js
+++ hello.js
@@ -5,3 +5,18 @@ const sayHello = function (name){
 
 //STEP 2: we called/invoked it using the parentheses notation.
 sayHello("Lauren");
+
+
+
+
+//return & console.log
+const sayHelloToConsole = function (name) {
+  console.log("Hello " + name);
+}
+sayHelloToConsole('John');
+
+const returnSayHello = function (name){
+  return "Hello" + name;
+}
+
+const greeting = returnSayHello('John');
